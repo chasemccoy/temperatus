7c8cc5bf9529ebc7b719d5ecc903046543be1c5d
Chase McCoy <chasem000@gmail.com>
2015-07-26 12:07:37 -0500

    Started implementing Wrapper class
    Defined some properties to hold data, and created methods to wrap the
    data. Also implemented pragma marks to organize the class. The app
    still compiles at this point, and responds to location changes well.
    Looking good.
    
    Can’t do much more until you sync your helper methods and day class.

--- Weather Widget/Weather Extension/Wrapper.h
+++ Weather Widget/Weather Extension/Wrapper.h
@@ -7,11 +7,34 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Forecastr.h"
 
 @interface Wrapper : NSObject
 
 @property (strong, nonatomic) NSDictionary *JSON;
 
--(Wrapper*)wrapData:(NSDictionary*)JSON;
+// Today
+@property (strong, nonatomic) NSString *currentSummary;
+@property (strong, nonatomic) NSString *currentTemp;
+@property (strong, nonatomic) NSString *currentFeelsLikeTemp;
+@property (strong, nonatomic) NSString *todayHighTemp;
+@property (strong, nonatomic) NSString *todayLowTemp;
+@property (strong, nonatomic) NSString *todayPrecipProbability;
+
+
+// Tomorrow
+@property (strong, nonatomic) NSString *tomorrowSummary;
+@property (strong, nonatomic) NSString *tomorrowHighTemp;
+@property (strong, nonatomic) NSString *tomorrowLowTemp;
+@property (strong, nonatomic) NSString *tomorrowPrecipProbability;
+
+// This Week
+@property (strong, nonatomic) NSString *weekSummary;
+@property (strong, nonatomic) NSArray *weekForecast;
+
+- (Wrapper*)wrapData:(NSDictionary*)JSON;
+- (void)wrapTodayData;
+- (void)wrapTomorrowData;
+- (void)wrapWeekData;
 
 @end
--- Weather Widget/Weather Extension/Wrapper.m
+++ Weather Widget/Weather Extension/Wrapper.m
@@ -8,11 +8,51 @@
 
 #import "Wrapper.h"
 
+NSString *const notAvailableText = @"N/A";
+
 @implementation Wrapper
 
 -(Wrapper*)wrapData:(NSDictionary*)JSON {
   _JSON = JSON;
+  [self wrapTodayData];
+  [self wrapTomorrowData];
+  [self wrapWeekData];
   return self;
+}
+
+
+#pragma mark - Wrap Today's Data
+
+- (void)wrapTodayData {
+  NSLog(@"\n******WRAPPING TODAY'S DATA******\n");
+  
+  NSDictionary *currently = _JSON[kFCCurrentlyForecast];
+  
+  NSString *summary           = currently[kFCSummary];
+  NSString *temp              = currently[kFCTemperature];
+  NSString *feelsLike         = currently[kFCApparentTemperature];
+  NSString *highTemp          = currently[kFCTemperatureMax];
+  NSString *lowTemp           = currently[kFCTemperatureMin];
+  NSString *precipProbability = currently[kFCPrecipProbability];
+  
+  _currentSummary = summary ? summary : notAvailableText;
+  _currentTemp = temp ? temp : notAvailableText;
+  _currentFeelsLikeTemp = feelsLike ? feelsLike : notAvailableText;
+  _todayHighTemp = highTemp ? highTemp : notAvailableText;
+  _todayLowTemp = lowTemp ? lowTemp : notAvailableText;
+  _todayPrecipProbability = precipProbability ? precipProbability : notAvailableText;
+}
+
+#pragma mark - Wrap Tomorrow's Data
+
+- (void)wrapTomorrowData {
+  NSLog(@"\n******WRAPPING TOMORROW'S DATA******\n");
+}
+
+#pragma mark - Wrap Weekly Data
+
+- (void)wrapWeekData {
+  NSLog(@"\n******WRAPPING WEEKLY DATA******\n");
 }
 
 @end
